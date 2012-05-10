3a9da973cf251385ec813ab5d8b3f37a420f560c
davis mcpherson <davis.mcpherson@gmail.com>
2012-05-10 18:07:34 -0400

    Initial commit for the sapp project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sapp::Application.config.secret_token = '1ed2005cb863387efd3656c5f1f70c3823c1e104be0cbf306a7fd67e1c6dc564736a654987eb1d0a3912ce7439bad0cf2ac20cb1a99321a73a723a1a41e65bdc'
